11cae187cef30a1a74f94cb8ae5328c3c86047e3
dylanhwood <dylanhwood04@gmail.com>
2021-06-25 19:06:46 -0700

    Final - Server side added

--- node/public/javascripts/script.js
+++ node/public/javascripts/script.js
@@ -1,40 +1,44 @@
 let list = []
+let id = 0
 class Show {
   constructor( pname, ptype, pstatus, prating, pgenre, plink) {
     this.name = pname;
     this.type = ptype;
     this.status = pstatus;
-    this.ID = list.length + 1;
     this.rating = prating;
     this.genre = pgenre;
     this.link = plink;
+    this.id = id++
     
   }
- 
-  getAll() {
-    return this.type + ': "' + this.name + '" &#8739; ' + 'Current Status: '+ this.status +  ' &#8739; Rating: ' + this.rating + ' &#8739; Genre: ' + this.genre;
-  }
 }
+ 
+  function genItem(item) {
+    return item.type + ': "' + item.name + '" &#8739; ' + 'Current Status: '+ item.status +  ' &#8739; Rating: ' + item.rating + ' &#8739; Genre: ' + item.genre;
+  }
 
 
 
 $(document).on("pagebeforeshow","#list",function() {
-  showEntries();
+  serverEntries();
 });
 
 $(document).on('pagebeforeshow','#stats',function() {
-  showValues();
+  serverVals();
 });
 
-//Append integer counter to amount of times an array value is repeated
-function showValues() {
-  if (list.length) {
-    let all = $('.stat').map(function() {
-      return this;
-    }).get();
-    all.forEach(item => {
-      item.innerHTML = ''
-    });
+
+
+function serverVals() {
+  $.get('/getList', function(data, status) {
+    list = data
+    if (list.length) {
+      let all = $('.stat').map(function() {
+        return this
+      }).get()
+      all.forEach(item => {
+        item.innerHTML = ''
+      })
 
     let numTelevision = list.reduce(((accumulator, currentValue) => currentValue.type !== 'Movie' && currentValue.status !== 'Plan to Watch' ? accumulator + 1 : accumulator), 0);
     $('#numTelevision').append(numTelevision);
@@ -61,11 +65,12 @@ function showValues() {
     $('#numScience').append(numScience);
     let numThriller = list.reduce(((accumulator, currentValue) => currentValue.genre == 'Thriller' ? accumulator + 1 : accumulator), 0);
     $('#numThriller').append(numThriller);
+    }
+  })
+}
 
-  }
-};
 
-function formSubmitEvent() {
+function newElement() {
   let programName = $('#programName').val();
   let type = $('#type').val();
   let status = $('#status').val();
@@ -73,54 +78,78 @@ function formSubmitEvent() {
   let genre = $('#genre').val();
   let link = $('#link').val();
   let show = new Show(programName, type, status, parseInt(rating), genre, link);
-  let result = true;
-  
-  if (result) {
-    list.push(show);
+
+  $.ajax({
+    url: '/add',
+    type: 'POST',
+    data: JSON.stringify(show),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function(result) {
+      console.log(result)
+    }
+  })
   $('#programName').val('');
   $('#link').val('');
-  } else {
-    alert("Invalid information. Try again.");
-  }
 }
-
 // Removes Child Elements that were filled in and add stores list elements
-function showEntries() {
-  let parent = $('#listId');
-  parent.empty(); 
-  list.forEach(item => {
-    let text = item.getAll();
+
+
+
+function serverEntries() {
+  let parent = $('#listId')
+  parent.empty() // Removes all existing child elements
+
+  $.get('/getList', function(data, status) {
+    list = data
+    console.log(data)
+    list.forEach(item => {
+      let text = genItem(item);
       if (item.link === '') {
         const search_query = item.name.replace(/\s/g, '+')
         item.link = 'https://www.youtube.com/results?search_query=' + search_query + ' trailer'
       }
-    
-    //Initialize Variables
-    let newText = document.createElement('li')
-    let deleteBtn = document.createElement('button')
-    //For css customization
-    deleteBtn.setAttribute("id", "delete");
-    let content = document.createElement('li')
+       //Initialize Variables
+      let newText = document.createElement('li')
+      let deleteBtn = document.createElement('button')
+      let listItem = document.createElement('li')
+      //For css customization
+      deleteBtn.setAttribute("id", "delete");
+      let content = document.createElement('li')
 
-    content.addEventListener('click', 
+      content.addEventListener('click', 
       function (event) {
         event.preventDefault()
-        if (confirm('You are about to open a new window. Please confirm.')) {
+        if (confirm('You are about to be redirected to a new window. PLease confirm.')) {
           window.open(item.link)
         }
       },
       false)
-    content.innerHTML = text
-    content.className = 'itemText'
-    deleteBtn.onclick = function() { 
-      let listIndex = Array.prototype.indexOf.call(newText.parentNode, newText)
-      list.splice(0, 1)
-      newText.parentNode.removeChild(newText) 
-    }
-    deleteBtn.innerHTML = 'X'
-    newText.appendChild(deleteBtn)
-    newText.appendChild(content)
-    newText.className = 'item'
-    parent.append(newText);
+    
+      content.innerHTML = text
+      content.className = 'itemText'
+      deleteBtn.onclick = function() {
+        let listIndex = item.id
+        $.ajax({
+          type: 'DELETE',
+          url: '/delete/' + listIndex,
+          success: function(result) {
+            newText.parentNode.removeChild(newText) 
+          },
+          error: function(xhr, textStatus, errorThrown) {
+            console.log('Error')
+            alert('This program does not have a valid ID.')
+          }
+        })
+      }
+      deleteBtn.innerHTML = 'X'
+  
+      newText.appendChild(deleteBtn)
+      newText.appendChild(content)
+      newText.className = 'item'
+  
+      // Add it to the unordered list
+      parent.append(newText);
+    })
   })
 }
--- node/routes/index.js
+++ node/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+dataList = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.sendFile('index.html');
+})
+
+router.get('/getList', function(req, res) {
+  res.status(200).json(dataList)
+})
+
+router.post('/add', function(req, res) {
+  const newProgram = req.body
+  dataList.push(newProgram)
+  res.status(200)
+})
+
+router.delete('/delete/:id', (req, res) => {
+  const id = req.params.id
+  let found = false
+  for(let i = 0; i < dataList.length; i++) {
+    if (dataList[i].id == id) {
+      dataList.splice(i, 1)
+      found = true
+      break;
+    }
+  }
+  if (!found) {
+    return res.status(500).json({
+      status: 'error'
+    })
+  } else {
+    res.status(200).json('deleted item')
+  }
+})
+
 
 module.exports = router;
